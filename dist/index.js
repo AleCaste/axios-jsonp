195af2149afe1b22ed03994063032ff3a35bdb73
AleCaste <alecaste@hotmail.com>
2017-09-02 08:50:05 -0500

    Added timeout support

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,104 @@
-!function(e,n){"object"==typeof exports&&"object"==typeof module?module.exports=n():"function"==typeof define&&define.amd?define([],n):"object"==typeof exports?exports.axiosJsonpAdapter=n():e.axiosJsonpAdapter=n()}(this,function(){return function(e){function n(o){if(t[o])return t[o].exports;var r=t[o]={i:o,l:!1,exports:{}};return e[o].call(r.exports,r,r.exports,n),r.l=!0,r.exports}var t={};return n.m=e,n.c=t,n.d=function(e,t,o){n.o(e,t)||Object.defineProperty(e,t,{configurable:!1,enumerable:!0,get:o})},n.n=function(e){var t=e&&e.__esModule?function(){return e.default}:function(){return e};return n.d(t,"a",t),t},n.o=function(e,n){return Object.prototype.hasOwnProperty.call(e,n)},n.p="",n(n.s=0)}([function(e,n){function t(e){var n=[];for(var t in e)n.push(encodeURIComponent(t)+"="+encodeURIComponent(e[t]));return n.join("&")}var o=1;e.exports=function(e){return new Promise(function(n,r){var a=document.createElement("script"),c=e.url;if(e.params){var i=t(e.params);i&&(c+=(c.indexOf("?")>=0?"&":"?")+i)}a.async=!0;var u="axiosJsonpCallback"+o++,d=window[u],p=!1;window[u]=function(e){if(window[u]=d,!p){n({data:e,status:200})}},c+=(c.indexOf("?")>=0?"&":"?")+t({callback:u,_:(new Date).getTime()}),a.onload=a.onreadystatechange=function(){a.readyState&&!/loaded|complete/.test(a.readyState)||(a.onload=a.onreadystatechange=null,a.parentNode&&a.parentNode.removeChild(a),a=null)},e.cancelToken&&e.cancelToken.promise.then(function(e){a&&(p=!0,r(e))}),a.src=c,document.head.appendChild(a)})}}])});
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+/*
+NOTE: this needs .indexOf and Promises support
+*/
+
+  var cid = 1;
+  var buildQueryString = function(params) {
+    var result = '';
+    var io1 = 0 - 1, ko1, vo1;
+    for (ko1 in params) { io1++; vo1 = params[ko1];
+      result += ((io1==0)?'':'&')+encodeURIComponent(ko1)+'='+encodeURIComponent(vo1);
+    }
+    return result;
+  };
+  module.exports = function jsonpAdapter(config) {
+  //window.jsonpAdapter = function(config) {
+    /* Specific config properties (same as in jQuery):
+       jsonp: 'callback'                          (specifies the callback parameter name, for jsonp the name of this param is 'callback' by default)
+       jsonpCallback: 'axiosJsonpCallback[cid]'   (specifies the callback parameter value, which for jsonp is the global function to execute with the data received e.g. axiosJsonpCallback12(dataMap) )
+    */
+    if (config==null)  config = {};
+      if (config.jsonp==null)  config.jsonp = 'callback';
+      if (config.jsonpCallback==null)  config.jsonpCallback = 'axiosJsonpCallback'+(cid++);
+    
+    return new Promise(function (resolve, reject) {
+      var query_string = '';
+      var script = document.createElement('script');
+      var src = config.url;
+      if (config.params) {
+        query_string = buildQueryString(config.params);
+        if (query_string!='') {
+          src += (
+            src.indexOf('?') >= 0
+              ? '&'
+              : '?'
+          ) + query_string;
+        }
+      }
+      script.async = true;
+      var old = window[config.jsonpCallback];
+      var isAbort = false;
+      window[config.jsonpCallback] = function(responseData) {
+        window[config.jsonpCallback] = old;
+        if (isAbort)  return;
+        var response = {
+          data  : responseData,
+          status: 200
+        };
+        resolve(response);
+      };
+      var params_jsonp = {};
+        params_jsonp[config.jsonp] = config.jsonpCallback; 
+        params_jsonp['_'] = (new Date().getTime()); 
+      query_string += (query_string==''?'':'&') + buildQueryString(params_jsonp);
+      src          += (src.indexOf('?')==-1?'?':'&') + query_string;
+      script.onload = script.onreadystatechange = function() {
+        if (!script.readyState || /loaded|complete/.test(script.readyState)) {
+          script.onload = script.onreadystatechange = null;
+          if (script.parentNode) {
+            script
+              .parentNode
+              .removeChild(script);
+          }
+          script = null;
+        }
+      };
+      if (config.cancelToken) {
+        config
+          .cancelToken
+          .promise
+          .then(function(cancel) {
+            if (!script)  return;
+            isAbort = true;
+            reject(cancel);
+          });
+      }
+      script.src = src;
+      document
+        .head
+        .appendChild(script);
+      if (config.timeout>0) {
+        setTimeout(function() {
+          reject('jsonp request timed out after '+config.timeout+' ms');
+        }, config.timeout);
+      }
+        
+    });
+  };
+},{}],2:[function(require,module,exports){
+/*
+@ Install browserify:
+npm install --global browserify
+@ Go to:
+D:\xampp\htdocs\http_server_es6
+@ Run:
+browserify ./index_browser.js -o ../dist/index.js
+*/ 
+window.jsonpAdapter = require('./index.js');
+
+
+
+
+
+},{"./index.js":1}]},{},[2]);
